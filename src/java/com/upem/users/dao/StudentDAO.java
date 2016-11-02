21c2013f7deef0aea9cadb2c213f5c9ec388df63
Davide Andrea Guastella <davide.guastella90@gmail.com>
2016-11-02 23:42:26 +0100

    - Added Create/Update/Delete methods for DAO

--- src/java/com/upem/users/dao/StudentDAO.java
+++ src/java/com/upem/users/dao/StudentDAO.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.upem.users.dao;
+
+import com.upem.users.entities.Student;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
  * @author Davide Andrea Guastella <davide.guastella90@gmail.com>
  */
+@Stateless
 public class StudentDAO {
-    
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private final static Logger logger = Logger.getLogger(StudentDAO.class.getName());
+
+    public void create(Student student) {
+        /* persist:
+            - Insert a new register to the database
+            - Attach the object to the entity manager.
+         */
+        logger.log(Level.INFO, "Adding student ID: " + student.getStudent_id());
+        em.persist(student);
+
+    }
+
+    public void update(Student student) {
+        /* merge:
+            - Find an attached object with the same id and update it.
+            - If exists update and return the already attached object.
+            - If doesn't exist insert the new register to the database.
+         */
+        logger.log(Level.INFO, "Updating student ID: " + student.getStudent_id());
+        em.merge(student);
+
+    }
+
+    public void delete(Student student) {
+        logger.log(Level.INFO, "Deleting student ID: " + student.getStudent_id());
+        em.remove(student);
+    }
 }
--- src/java/com/upem/users/dao/TeacherDAO.java
+++ src/java/com/upem/users/dao/TeacherDAO.java
@@ -1,6 +1,11 @@
 package com.upem.users.dao;
 
+import com.upem.users.entities.Teacher;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -8,5 +13,35 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class TeacherDAO {
-    
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private final static Logger logger = Logger.getLogger(TeacherDAO.class.getName());
+
+    public void create(Teacher teacher) {
+        /* persist:
+            - Insert a new register to the database
+            - Attach the object to the entity manager.
+         */
+        logger.log(Level.INFO, "Adding teacher ID: " + teacher.getTeacher_id());
+        em.persist(teacher);
+
+    }
+
+    public void update(Teacher teacher) {
+        /* merge:
+            - Find an attached object with the same id and update it.
+            - If exists update and return the already attached object.
+            - If doesn't exist insert the new register to the database.
+         */
+        logger.log(Level.INFO, "Updating teacher ID: " + teacher.getTeacher_id());
+        em.merge(teacher);
+
+    }
+
+    public void delete(Teacher teacher) {
+        logger.log(Level.INFO, "Deleting teacher ID: " + teacher.getTeacher_id());
+        em.remove(teacher);
+    }
 }
