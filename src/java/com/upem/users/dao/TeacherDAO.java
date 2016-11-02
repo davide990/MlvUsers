422df9c9a69098e755f37c1ad93e21899ed7a33a
Davide Andrea Guastella <davide.guastella90@gmail.com>
2016-11-02 23:32:54 +0100

    - Project created

---
+++ src/java/com/upem/users/dao/TeacherDAO.java
@@ -0,0 +1,12 @@
+package com.upem.users.dao;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Davide Andrea Guastella <davide.guastella90@gmail.com>
+ */
+@Stateless
+public class TeacherDAO {
+    
+}
---
+++ src/java/com/upem/users/entities/Teacher.java
@@ -0,0 +1,81 @@
+package com.upem.users.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Davide Andrea Guastella <davide.guastella90@gmail.com>
+ */
+@Entity
+@Table(name = "teacher")
+public class Teacher implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "teacher_id")
+    private long teacher_id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "birth_date")
+    private String birth_date;
+
+    public long getTeacher_id() {
+        return teacher_id;
+    }
+
+    public void setTeacher_id(long teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(String birth_date) {
+        this.birth_date = birth_date;
+    }
+
+}
